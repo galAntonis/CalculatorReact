1762bcd5a4342b8018fc4c6a74d497005275d79a
Antonis Galeos <galant.dev@gmail.com>
2022-12-05 23:54:21 +0200

    Added title

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 
 
 function App() {
-  return (
-    <div className="App">
-        <div className="test2"></div>
-    </div>
-  );
+	return (
+		<div className="App">
+				<div className="headerTitle">
+					<span><p>hover or tap me</p></span>
+				</div>
+				<div className="calculator">
+
+				</div>
+		</div>
+	);
 }
 
 export default App;
